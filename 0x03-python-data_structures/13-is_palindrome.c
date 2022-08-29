f84ee699cdba8e12501ba272e4d5f7b01edc0e3d
senaitfikadu <senaitfikadu44@gmail.com>
2022-08-29 04:18:46 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,54 @@
 #include "lists.h"
 
 /**
-  * is_palindrome - checks if a singly linked list is a palindrome
-  * @head: double pointer to list.
-  *
-  *Return: 1 if palindrome, 0 if not palindrom.
-  */
-int is_palindrome(listint_t **head)
+* list_len - finds no. of elements ina linked list.
+* @h: pointer to linked list.
+*
+* Return: number of elements in linked list.
+*/
+size_t list_len(listint_t *h)
 {
-	return (checkPalindrome(head, *head));
+	size_t  nodes = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
 
 /**
-  * checkPalindrome - recursive function ot check if sinly linked list
-  * is a palindrome.
-  * @headptr: double pointer to list.
-  * @tptr: pointer to list.
-  *
-  * Return: 1 or 0
-  */
-int checkPalindrome(listint_t **headptr, listint_t *tptr)
+* is_palindrome - checks if a singly linked list is a palindrome.
+* @head: double pointert to head of d-list.
+*
+* Return: 1 if palindrome, 0 otherwise.
+*/
+int is_palindrome(listint_t **head)
 {
-	int res;
+	int *nArr, i = 0, j = 0, len = 0;
+	listint_t *temp;
 
-	/* base case */
-	if (tptr == NULL)
+	if (*head == NULL)
 		return (1);
-	res = checkPalindrome(headptr, tptr->next) && ((*headptr)->n == tptr->n);
-	return (res);
+	temp = *head;
+	len = list_len(temp);
+	nArr = (int *)malloc(sizeof(int) * len);
+	if (nArr == NULL)
+		return (2);
+	temp = *head;
+	while (temp != NULL)
+	{
+		nArr[j] = temp->n;
+		j++;
+		temp = temp->next;
+	}
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		if (nArr[i] != nArr[j])
+			return (0);
+	}
+	return (1);
 }
